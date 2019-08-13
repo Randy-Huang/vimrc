70ead2543620a708f82e87c1f322be680af3f648
Randy Huang <randy.hunang@gmail.com>
2019-08-13 14:16:48 +0800

    Update install-vimrc.sh

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+cd ~/.vim/colors/
+ln -s ~/.vim/bundle/wombat256.vim/colors/wombat256mod.vim wombat265.vim
+ln -s ~/.vim/bundle/Solarized/colors/solarized.vim solarized.vim
+
 cd ~
 ln -s .vim/vimrc .vimrc
 
